e342fb84843e7d659f734d663f20b3c2ad8bbe72
Rutuja412 <rutujakadam4545@gmail.com>
2023-06-16 09:51:49 +0530

    [Rutuja]:Welcome

---
+++ Constructor-Interface-AccessModifiers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Constructor_Interface_AccessModifiers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Constructor_Interface_Access Modifiers");
+        }
+    }
+}
