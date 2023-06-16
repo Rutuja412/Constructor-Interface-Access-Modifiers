409f3dde168740059567ae0dc36e2fc3596950fe
Rutuja412 <rutujakadam4545@gmail.com>
2023-06-16 09:56:52 +0530

    [Rutuja]:Types of constructor

--- Constructor-Interface-AccessModifiers/Program.cs
+++ Constructor-Interface-AccessModifiers/Program.cs
@@ -7,6 +7,11 @@ namespace Constructor_Interface_AccessModifiers
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Constructor_Interface_Access Modifiers");
+            Console.WriteLine("Constructor");
+            Constructor constructor = new Constructor();//Prameterless
+            Console.WriteLine("Default value of a : " + constructor.a);//defualt
+            Constructor2 constructor2 = new Constructor2(102, "Rutuja");//Parameterized
+            constructor2.Display();
         }
     }
 }
